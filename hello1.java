807707ff10778024f96a75dda1156b98b517c125
vijaylaxmi-k <vvkulkarni1992@gmail.com>
2021-08-02 15:18:13 +0530

    added new Vijaylaxmi name

--- hello1.java
+++ hello1.java
@@ -5,8 +5,11 @@ public class Hello {
 	public static void main(String[] args) {
 <<<<<<< HEAD
 		System.out.println("hi changes");
-=======
+======
+
 		System.out.println("hello");
-                 System.out.println("how r u"):
+                 System.out.println("how r u");
+		 System.out.println("vijaylaxmi-k");
 	}
-}
+} 
+
