94dedcfa95848b5a21a469c6bc5e21fb5c0bb93d
vijaylaxmi-k <vvkulkarni1992@gmail.com>
2021-08-02 11:38:02 +0530

    Git Practise session1

---
+++ Hello.java
@@ -0,0 +1,10 @@
+package myPack;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+
+	}
+
+}
---
+++ Plusing.java
@@ -0,0 +1,23 @@
+package myPack;
+
+public class Plusing {
+		float PI=22/7 ;
+		
+public int add(int a, int b)
+{
+	return a+b;
+}
+public int sub(int a, int b)
+{
+	return a-b;
+}
+
+	public static void main(String[] args) 
+	{
+		Plusing obj = new Plusing();
+		System.out.println("add of numb:" +obj.add(5, 10));
+		System.out.println("add of numb:" +obj.sub(5, 10));
+
+	}
+
+}
